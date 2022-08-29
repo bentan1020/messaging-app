5cee16ad8e1e4d8a8afa90b359f98d439a6b76bb
ben <73725152+bentan1020@users.noreply.github.com>
2022-08-29 10:10:08 -0400

    Css updated

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -19,7 +19,7 @@ function Chat() {
       <div className='msgs'>
         {messages.map(({id, text, photoURL, uid}) => (
           <div>
-            <div key={id} className={`msg ${uid == auth.currentUser ? 'sent' : 'received'}`}>
+            <div key={id} className={`msg ${uid == auth.currentUser.uid ? 'sent' : 'received'}`}>
               <img src={photoURL} alt="image"></img>
               <p>{text}</p>
             </div>
