bb08afa9c16053420d9896504666f79d97ca17cf
ben <73725152+bentan1020@users.noreply.github.com>
2022-08-30 11:21:07 -0400

    some updates

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,7 +2,7 @@ import React from 'react'
 import firebase from 'firebase/compat/app';
 import { auth } from '../firebase.js'
 
-import { Button, Typography, Box, Grid } from '@material-ui/core'
+import { Button, Typography, Box} from '@material-ui/core'
 import MessageRoundedIcon from '@mui/icons-material/MessageRounded';
 import GoogleIcon from '@mui/icons-material/Google';
 
@@ -15,27 +15,26 @@ function SignIn() {
 
     return (
         <>
-            <Grid container spacing={3} style={{width:'100%'}}>
-                <Grid xs={0} md={4}>
-
-                </Grid>
-                <Grid xs={0} md={4}>
-                    <div className="flex flex-col text-center">
-                        <Box pt={8} pb={3}>
-                            <MessageRoundedIcon color='primary' sx={{ fontSize: 100 }}></MessageRoundedIcon>
-                        </Box>
-                        <Box pt={3} pb={3}>
+            <div className="flex bg-yellow-500 h-screen">
+                <div className='flex-1'></div>
+                <div className='flex-1 py-32 px-72'>
+                    <div className="flex flex-col text-center bg-yellow-100 rounded-3xl">
+                        <Box pt={5}>
                             <Typography variant='h5'>React Message Chat</Typography>
                         </Box>
-                        <Box pt={3} pb={3}>
+                        <Box pt={2}>
+                            <Typography variant='h8'>Sign Into Your Account</Typography>
+                        </Box>
+                        <Box py={3}>
+                            <MessageRoundedIcon color='primary' sx={{ fontSize: 100 }}></MessageRoundedIcon>
+                        </Box>
+                        <Box py={3}>
                             <Button variant="contained" startIcon={<GoogleIcon/>} color="primary" onClick={signInWithGoogle}>Sign In With Google</Button>
                         </Box>
                     </div>
-                </Grid>
-                <Grid xs={0} md={4}>
-
-                </Grid>
-            </Grid>
+                </div>
+                <div className='flex-1'></div>
+            </div>
         </>
     )
 }
