83c16c624e92a957518cf7e02c26db4109150c9d
ben <73725152+bentan1020@users.noreply.github.com>
2022-08-30 22:34:33 -0400

    sign in page css

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,9 +2,20 @@ import React from 'react'
 import firebase from 'firebase/compat/app';
 import { auth } from '../firebase.js'
 
-import { Button, Typography, Box} from '@material-ui/core'
+import { Button, Typography, Box, Paper } from '@material-ui/core'
 import MessageRoundedIcon from '@mui/icons-material/MessageRounded';
 import GoogleIcon from '@mui/icons-material/Google';
+
+import signInBackground from "../assets/background.jpeg"
+
+const styles = {
+    paperContainer: {
+        backgroundImage: `url(${signInBackground})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+    }
+};
 
 function SignIn() {
 
@@ -15,26 +26,28 @@ function SignIn() {
 
     return (
         <>
-            <div className="flex bg-yellow-500 h-screen">
-                <div className='flex-1'></div>
-                <div className='flex-1 py-32 px-72'>
-                    <div className="flex flex-col text-center bg-yellow-100 rounded-3xl">
-                        <Box pt={5}>
-                            <Typography variant='h5'>React Message Chat</Typography>
-                        </Box>
-                        <Box pt={2}>
-                            <Typography variant='h8'>Sign Into Your Account</Typography>
-                        </Box>
-                        <Box py={3}>
-                            <MessageRoundedIcon color='primary' sx={{ fontSize: 100 }}></MessageRoundedIcon>
-                        </Box>
-                        <Box py={3}>
-                            <Button variant="contained" startIcon={<GoogleIcon/>} color="primary" onClick={signInWithGoogle}>Sign In With Google</Button>
-                        </Box>
-                    </div>
-                </div>
-                <div className='flex-1'></div>
-            </div>
+            <Paper style={styles.paperContainer}>
+                <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' }}>
+                    <Box></Box>
+                    <Box sx={{ p:10 }}> 
+                        <div className="flex flex-col text-center h-screen pt-32 bg-slate-50">
+                            <Box pt={5}>
+                                <Typography variant='h5'>React Message Chat</Typography>
+                            </Box>
+                            <Box pt={2}>
+                                <Typography variant='h8'>Sign Into Your Account</Typography>
+                            </Box>
+                            <Box py={3}>
+                                <MessageRoundedIcon color='primary' sx={{ fontSize: 100 }}></MessageRoundedIcon>
+                            </Box>
+                            <Box py={3}>
+                                <Button variant="contained" startIcon={<GoogleIcon/>} color="primary" onClick={signInWithGoogle}>Sign In With Google</Button>
+                            </Box>
+                        </div> 
+                    </Box>
+                    <Box></Box>
+                </Box>
+            </Paper>
         </>
     )
 }
